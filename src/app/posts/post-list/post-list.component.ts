2ab4c2eb7396b5f32ff78b11e3503e25c245095b
Bormoley1983 <zhuravel_leonid@yahoo.com>
2025-10-04 11:09:02 +0300

    loading fix

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -23,7 +23,7 @@ export class PostListComponent implements OnInit, OnDestroy {
     this.postsSub = this.postsService
       .getPostUpdateListener()
       .subscribe((posts: Post[]) => {
-        this.isLoading = true;
+        this.isLoading = false;
         this.posts = posts;
         console.log('PostListComponent ngOnInit - posts updated:', posts);
       });
