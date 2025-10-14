c68984d6e234e8c3fd07a1d273dea9d3eeed0dd7
Bormoley1983 <zhuravel_leonid@yahoo.com>
2025-10-14 11:55:00 +0300

    p10 error handling

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { AuthService } from '../auth.service';
 
@@ -8,10 +9,19 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./signup.component.scss'],
   standalone: false,
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit, OnDestroy {
   isLoading = false;
+  private authStatusSub!: Subscription;
 
   constructor(public authService: AuthService) {}
+
+  ngOnInit() {
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe(authStatus => {
+        this.isLoading = false;
+      });
+  }
 
   onSignup(form: NgForm) {
     if (form.invalid) {
@@ -20,5 +30,9 @@ export class SignupComponent {
     this.isLoading = true;
     console.log(form.value);
     this.authService.createUser(form.value.email, form.value.password);
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
   }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -63,9 +63,14 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   onDeletePost(postId: string) {
     this.isLoading = true;
-    this.postsService.deletePost(postId).subscribe(() => {
-      this.postsService.getPosts(this.postsPerPage, this.currentPage);
-    });
+    this.postsService.deletePost(postId).subscribe(
+      () => {
+        this.postsService.getPosts(this.postsPerPage, this.currentPage);
+      },
+      () => {
+        this.isLoading = false;
+      }
+    );
   }
 
   ngOnDestroy(): void {
